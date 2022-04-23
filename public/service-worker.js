c4d7754ca5f15470bfef8c39cb7fc2c6b1e0590c
Thoma77s <thomassiffc3@gmail.com>
2022-04-22 20:05:00 -0400

    Add manifest.json to file, added icons, and adjusted service worker

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,12 +1,12 @@
 const FILES_TO_CACHE = [
   "/",
-  "manifest.webmanifest",
-  "index.html",
-  "assets/css/styles.css",
-  "assets/js/index.js",
-  "assets/js/db.js",
-  "assets/images/icons/icon-192x192.png",
-  "assets/images/icons/icon-512x512.png",
+  "manifest.json",
+  "public/index.html",
+  "public/css/styles.css",
+  "public/js/index.js",
+  "public/js/idb.js",
+  "public/icons/icon-192x192.png",
+  "public/icons/icon-512x512.png",
 ];
 
 const CACHE_NAME = "static-cache-v2";
